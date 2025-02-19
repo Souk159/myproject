80cf7eb81ad33edbb46c1c9c9ea54b09ddcb2cd0
Souk159 <soukthunva152@gmail.com>
2025-02-20 00:37:51 +0700

    updatemyproject

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={HeroImage} alt="" className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105'/>
         <h1 className='text-4xl font-bold'>
             I'm {" "}
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Soukthuva Maopasert </span>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Soukthunva Maopasert </span>
             Software Developers
         </h1>
         <p className='mt-4 text-lg text-gray-300 '>
